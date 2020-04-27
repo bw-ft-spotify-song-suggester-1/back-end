58f47d01e276d20d6a4432de77c04fd389095d5d
anthony2698 <carrilloanthony2698@gmail.com>
2020-04-27 17:01:38 -0400

    Added branch.

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,4 +3,4 @@ const knexConfig = require('../knexfile.js');
 
 const environment = process.env.DB_ENV || 'development';
 
-module.exports = knex(knexConfig.environment);
+module.exports = knex(knexConfig[environment]);

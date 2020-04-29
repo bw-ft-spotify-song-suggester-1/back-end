632063e4d46cffcb3d409436c6452b955a236a5f
gavin stahl <stahlgavin@gmail.com>
2020-04-29 00:06:41 -0700

    routes ready for favorites and middleware, need to finish logic for endpoints

--- api/server.js
+++ api/server.js
@@ -6,12 +6,14 @@ const server = express();
 // const authenticate = require('../auth/auth-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const userRouter = require('../users/users-router.js');
+// const favRouter = require('../favorites/favorites-router.js');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/users', userRouter);
+// server.use('/api/favorites', favRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up and running! go to /api/users /api/auth/register /api/auth/login for some requests!" });

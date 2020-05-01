07d6f3c4cbc2a652b9a89275c798e2f29c5340c7
gavin stahl <stahlgavin@gmail.com>
2020-04-30 18:24:43 -0700

    registration tests

---
+++ auth/auth.spec.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const server = require("../api/server");
+const db = require("../database/dbConfig.js");
+
+describe("registration functional", function () {
+  describe("/auth/register", function () {
+    beforeEach(async () => {
+      await db("users").truncate();
+    });
+    it("should return status code 201", function () {
+      return request(server)
+        .post("/api/auth/register")
+        .send({ username: "gavtesting", password: "gavtesting" })
+        .then((res) => {
+          expect(res.status).toBe(201);
+        });
+    });
+    it("should have added user to db", async function () {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "gavtesting", password: "gavtesting" })
+        .then((res) => {
+          expect(res.body.username).toBe("gavtesting");
+        });
+      const inserted = await db("users");
+      expect(inserted).toHaveLength(1);
+    });
+  });
+});

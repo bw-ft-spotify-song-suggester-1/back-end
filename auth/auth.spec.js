01b493504e3f87b5b77c5c60fe069b084785c3af
gavin stahl <stahlgavin@gmail.com>
2020-04-30 23:24:47 -0700

    more tests added

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -7,7 +7,7 @@ describe("registration functional", function () {
     beforeEach(async () => {
       await db("users").truncate();
     });
-    it("should return status code 201", function () {
+    it("should post successfully with status 201", function () {
       return request(server)
         .post("/api/auth/register")
         .send({ username: "gavtesting", password: "gavtesting" })
@@ -22,18 +22,33 @@ describe("registration functional", function () {
         .then((res) => {
           expect(res.body.username).toBe("gavtesting");
         });
+    });
+    it("should have added user to db", async function () {
+      await request(server)
+        .post("/api/auth/register")
+        .send({ username: "gavtesting", password: "gavtesting" })
+        .then((res) => {
+          console.log("user registration successful");
+        });
       const inserted = await db("users");
       expect(inserted).toHaveLength(1);
     });
-    it("should have added user to db", async function () {
+  });
+});
 
-        await request(server)
-          .post("/api/auth/register")
-          .send({ username: "gavtesting", password: "gavtesting" })
-          .then((res) => {
-          });
-          const inserted = await db("users");
-          expect(inserted).toHaveLength(1);
-      });
+describe("logion functional", function () {
+  describe("/auth/login", function () {
+    // beforeEach(async () => {
+    //   await db("users").truncate();
+    // });
+    it("should return user's token and status 200", function () {
+      return request(server)
+        .post("/api/auth/login")
+        .send({ username: "gavtesting", password: "gavtesting" })
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.body.token).toBeTruthy();
+        });
+    });
   });
 });
